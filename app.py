3930c9d52ce173f77dffa94768e037695cc9ecca
JordanTheDodger <54824287+JordanTheDodger@users.noreply.github.com>
2019-11-17 21:56:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,67 +1,58 @@
-# "with" is a construct that allows to open a file, you can write on it or read from it and it closes automatically.
-# it also allows to assign file with mode to a variable that can be treated as file variable
+"""'with is a construct that allows to open a file, you can write on it or read from it and it closes automatically.
+it also allows to assign file with mode to a variable that can be treated as file variable"""
+import os
 import json
 from user import User
 
 
+def menu():
+    user_name = input('please enter user name: ')
 
-'''laoding data to the file using json.load()'''
-#to save directly to a file we can need to use json libr so we imported json
-# with open('Jordan.txt','r') as f:
-#     json_data = json.load(f)  # json_data becomes now  a python dictionary
-#     user = User.from_json(json_data)
-#     print(user.json())
+    filename = f'{user_name}.txt'
+    if file_exists(filename):
+        print(f'Welcome member {user_name}')
+        with open(filename, 'r') as usr_file:
+            json_data = json.load(usr_file)
+        user = User.from_json(json_data)
 
+    else:
+        user = User(user_name)
+        print(f'Welcome {user_name}')
 
+    user_input = input(
+        "Enter 'a' to add a movie, 's' to see the list of movies, 'w' to set a movie watched, 'd' delete a movie, "
+        "'l' to see the list of watched movies or 'f' to save and  'q' to  quit ")
 
+    while user_input != 'q':
+        if user_input == 'a':
+            movie_name = input('enter movie name')
+            movie_genre = input('enter movie genre')
+            user.add_movie(movie_name, movie_genre)
+        elif user_input == 's':
+            for movie in user.movies:
+                print(f'Name: {movie.name} Genre: {movie.genre} Watched: {movie.watched}')
+        elif user_input == 'w':
+            watched_movie = input('enter the movie name to set as watched')
+            user.set_watched(watched_movie)
+        elif user_input == 'd':
+            delete_movie = input('enter movie name to delete: ')
+            user.delete_movie(delete_movie)
+        elif user_input == 'l':
+            for movie in user.movies:
+                print('Name: {} genre:{} worked:{}'.format(movie.name, movie.genre, movie.watched))
+        elif user_input == 'f':
+            with open(filename, 'w') as f:
+                json.dump(user.json(), f)
 
-
-
-
-
-'''wiritng onto a file using json.dump()'''
-# writing on to the file
-user=User('James')
-user.add_movie('Avenger','comic')
-user.add_movie('Rush','Crime')
-print(user.json())
-with open('Jordan.txt','w') as f:
-    json.dump(user.json(), f)  # pass a file pointer{while calling change file opening mode}
-
-
-
-
-
-
-
-'''Saving and laoding data in CSV format'''
-'''loading from a file'''
-# loading from a file
-# loading_data_from_file= user.load_from_file('Jordan.txt')
-# print(loading_data_from_file.movies)
-
-
-''' writing to a file'''
-# user.add_movie('inception','sic-fi')
-# user.add_movie('aquaman','comic')
-#
-# user.save_to_file()
-''' '''
-
-''''"with" contstructor '''
-# with open("my_file.txt") as f:
-#     f.write("Learning python")
-#     print(f.readline())
+        user_input = input(
+            "Enter 'a' to add a movie, 's' to see the list of movies, 'w' to set a movie watched, 'd' delete a movie, "
+            "'l' to see the list of watched movies or 'q' to save and quit ")
 
 
-''' testing appending movies'''
-# from movie import Movie
-# from user import User
-#
-# user=User("jordan")
-# my_movie=Movie('abc','ncd',True)
-# user.movies.append(my_movie)
-#
-#
-#
-# print(user.watched_movies())
+def file_exists(filename):
+    return os.path.isfile(filename)
+
+
+menu()
+
+
